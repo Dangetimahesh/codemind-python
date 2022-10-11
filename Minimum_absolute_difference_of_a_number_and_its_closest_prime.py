c5fdadc0ede2586bcb4e92bd1151e66e26b360dd
Mahesh <84697586+Dangetimahesh@users.noreply.github.com>
2022-10-11 15:38:14 +0530

    n=int(input()) next_prime=n while True:     fc=0     for i in range(1,next_prime+1):         if next_prime%i==0:             fc+=1     if fc==2:         break     next_prime+=1
    prev_prime=n
    while True:
        fc=0
        for i in range(1,prev_prime+1):
              if prev_prime%i==0:
                    fc+=1
        if fc==2:
            break
        prev_prime-=1
            
    if n-prev_prime<next_prime-n:
        print(n-prev_prime)
    else:
        print(next_prime-n)

---
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -0,0 +1,25 @@
+n=int(input())
+next_prime=n
+while True:
+    fc=0
+    for i in range(1,next_prime+1):
+        if next_prime%i==0:
+            fc+=1
+    if fc==2:
+        break
+    next_prime+=1
+
+prev_prime=n
+while True:
+    fc=0
+    for i in range(1,prev_prime+1):
+          if prev_prime%i==0:
+                fc+=1
+    if fc==2:
+        break
+    prev_prime-=1
+        
+if n-prev_prime<next_prime-n:
+    print(n-prev_prime)
+else:
+    print(next_prime-n)
